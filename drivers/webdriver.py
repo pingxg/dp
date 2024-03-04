e76fc01ee97150cef8f8fe3f7e52ced07af8fdd0
Pingxin Gao <gaopingxin0315@gmail.com>
2024-03-04 15:55:43 +0200

    change back the headless mode

--- drivers/webdriver.py
+++ drivers/webdriver.py
@@ -26,7 +26,7 @@ def setup_driver(download_path=None):
     }
     chrome_options.add_experimental_option("prefs", prefs)
     chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument('--disable-gpu')
